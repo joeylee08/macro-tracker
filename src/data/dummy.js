936cc56d2fd17d973bd8ba8ada40be0dbbacceae
Joseph <josephylee72@gmail.com>
2026-08-21 13:06:57 -0700

    dummy data

---
+++ src/data/dummy.js
@@ -0,0 +1,37 @@
+let users = [
+    { id: 1, username: 'joeylee08', firstName: 'Joseph', lastName: 'Lee', dateJoined: '', calGoal: 0, proteinGoal: 0, carbGoal: 0, fatGoal: 0, sodiumGoal: 0},
+    { id: 2, username: 'demonslayer666', firstName: 'Bobert', lastName: 'Smith', dateJoined: '', calGoal: 0, proteinGoal: 0, carbGoal: 0, fatGoal: 0, sodiumGoal: 0},
+    { id: 3, username: 'wildgirl69', firstName: 'Katie', lastName: 'Contreras', dateJoined: '', calGoal: 0, proteinGoal: 0, carbGoal: 0, fatGoal: 0, sodiumGoal: 0},
+    { id: 4, username: 'littlekidlover', firstName: 'Michael', lastName: 'Scott', dateJoined: '', calGoal: 0, proteinGoal: 0, carbGoal: 0, fatGoal: 0, sodiumGoal: 0},
+    { id: 5, username: 'prisonbuddy', firstName: 'Abraham', lastName: 'Rincon', dateJoined: '', calGoal: 0, proteinGoal: 0, carbGoal: 0, fatGoal: 0, sodiumGoal: 0},
+    { id: 6, username: 'oregondude12', firstName: 'Taylor', lastName: 'Treehugger', dateJoined: '', calGoal: 0, proteinGoal: 0, carbGoal: 0, fatGoal: 0, sodiumGoal: 0},
+    { id: 7, username: 'therealbobhope', firstName: 'Bob', lastName: 'Hope', dateJoined: '', calGoal: 0, proteinGoal: 0, carbGoal: 0, fatGoal: 0, sodiumGoal: 0},
+    { id: 8, username: 'ibench405', firstName: 'Yeah', lastName: 'Bruh', dateJoined: '', calGoal: 0, proteinGoal: 0, carbGoal: 0, fatGoal: 0, sodiumGoal: 0},
+]
+
+let meals = [
+    {id: 1, name: 'Prison Oatmeal', userId: 1, cal: 0, protein: 0, carb: 0, fat: 0, sodium: 0, ingredients: []},
+    {id: 2, name: 'Chicken Avocado Wrap', userId: 1, cal: 0, protein: 0, carb: 0, fat: 0, sodium: 0, ingredients: []},
+    {id: 3, name: 'Egg Avocado Wrap', userId: 1, cal: 0, protein: 0, carb: 0, fat: 0, sodium: 0, ingredients: []},
+    {id: 4, name: 'Salmon Quinoa Kimchi Bowl', userId: 1, cal: 0, protein: 0, carb: 0, fat: 0, sodium: 0, ingredients: []},
+    {id: 5, name: 'Chicken Teriyaki Bowl', userId: 1, cal: 0, protein: 0, carb: 0, fat: 0, sodium: 0, ingredients: []},
+    {id: 6, name: 'Turkey Breakfast Burrito', userId: 1, cal: 0, protein: 0, carb: 0, fat: 0, sodium: 0, ingredients: []},
+]
+
+let foods = [
+    {id: 1, name: 'Chicken Breast', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 2, name: 'Canned Tuna', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 3, name: 'Eggs', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 4, name: '2% Milk', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 5, name: 'Oatmeal', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 6, name: 'Peanut Butter', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 7, name: 'Honey', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 8, name: 'Banana', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 9, name: 'Avocado', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 10, name: 'Salmon', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 11, name: 'Quinoa', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0}
+    {id: 12, name: 'Flour Tortilla', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 13, name: 'Corn Tortilla', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 14, name: 'Egg White', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+    {id: 15, name: 'Tofu', calPerOz: 0, proteinPerOz: 0, carbPerOz: 0, fatPerOz: 0, sodiumPerOz: 0, amount: 0},
+]
