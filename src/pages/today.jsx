7712aab568b1990c3e9191c7f4dbd59ee85008fb
Joseph <josephylee72@gmail.com>
2026-08-22 09:40:02 -0700

    navlinks modified

--- src/pages/today.jsx
+++ src/pages/today.jsx
@@ -1,6 +1,9 @@
+import NavBar from "../components/NavBar"
+
 const Today = () => {
     return (
         <>
+            <NavBar/>
             <h1>Your Daily Totals</h1>
             <p>What have you had today?</p>
         </>
