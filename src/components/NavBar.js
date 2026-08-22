7423032e5b172079e601ba3cd7c61ccdc71adf70
Joseph <josephylee72@gmail.com>
2026-08-22 09:34:05 -0700

    navbar and routes fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
   return (
     <nav className='navbar'>
       <ul className='linksList'>
-        <li><NavLink to='/'>className={({isActive}) => isActive ? 'selected' : null}<span>Today</span></NavLink></li>
-        <li><NavLink to='/menu'>className={({isActive}) => isActive  ? 'selected' : null}<span>Menu</span></NavLink></li>
-        <li><NavLink to='/history'>className={({isActive}) => isActive  ? 'selected' : null}<span>History</span></NavLink></li>
-        <li><NavLink to='/friends'>className={({isActive}) => isActive  ? 'selected' : null}<span>Friends</span></NavLink></li>
+        <li><NavLink to='/' className={({isActive}) => isActive ? 'selected' : null}><span>Today</span></NavLink></li>
+        <li><NavLink to='/menu' className={({isActive}) => isActive  ? 'selected' : null}><span>Menu</span></NavLink></li>
+        <li><NavLink to='/history' className={({isActive}) => isActive  ? 'selected' : null}><span>History</span></NavLink></li>
+        <li><NavLink to='/friends' className={({isActive}) => isActive  ? 'selected' : null}><span>Friends</span></NavLink></li>
       </ul>
     </nav>
   )
