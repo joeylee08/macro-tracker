c48a6d87ab6ac87debebcffa5288d93e3a043a67
Joseph <josephylee72@gmail.com>
2026-08-22 08:49:37 -0700

    replace home with today

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/home';
 import Today from './pages/today';
 import Menu from './pages/menu';
 import History from './pages/history';
@@ -11,8 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/today' element={<Today />} />
+        <Route path='/' element={<Today />} />
         <Route path='/menu' element={<Menu />} />
         <Route path='/history' element={<History />} />
         <Route path='/friends' element={<Friends />} />
