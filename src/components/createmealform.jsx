83b79b151fef8149f3fbb0b61e74bb3cb47788cd
Joseph <josephylee72@gmail.com>
2026-08-28 14:10:27 -0700

    mealForm started

--- src/components/createmealform.jsx
+++ src/components/createmealform.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 
 const CreateMealForm = ({ onAddMeal, currentUserId, nextMealIndex, myFoods }) => {
-    const [formData, setFormData] = useState({
+    const [meal, setMeal] = useState({
         id: nextMealIndex,
         name: '',
         userId: currentUserId,
         ingredients: []
     })
 
-    function handleChange(event) {
+    function handleMealChange(event) {
         const { name, value } = event.target
+
+    }
+
+    const [ingredient, setIngredient] = useState({
+        foodId: null,
+        units: 1
+    })
+
+    function handleIngredientChange(event) {
 
     }
 
@@ -24,13 +33,15 @@ const CreateMealForm = ({ onAddMeal, currentUserId, nextMealIndex, myFoods }) =>
     )
 
     return (
-        <form className='form' onChange={handleChange} onSubmit={handleSubmit}>
+        <form className='form' onChange={handleMealChange} onSubmit={handleSubmit}>
             <label htmlFor='name'>Meal Name</label>
-            <input type='text' id='name' name='name' value={formData.name} required minLength='1' maxLength='26'></input>
+            <input type='text' id='name' name='name' value={meal.name} required minLength='1' maxLength='26'></input>
             <label htmlFor='ingredients'>Ingredients</label>
             <select id='ingredients' name='ingredients'>
                 {ingredients}
             </select>
+            <label htmlFor='units'>Units</label>
+            <input type='number' id='units' name='units' required min='0.1' step='any'></input>
         </form>
     )
 }
