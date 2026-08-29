a2ffdd14fd2a8a4088295270836ba69ef435026b
Joseph <josephylee72@gmail.com>
2026-08-28 20:16:46 -0700

    foodId === 0 error handling

--- src/components/createmealform.jsx
+++ src/components/createmealform.jsx
@@ -20,7 +20,7 @@ const CreateMealForm = ({ onAddMeal, currentUserId, nextMealIndex, myFoods }) =>
     //ingredient model
     const [ingredient, setIngredient] = useState({
         foodId: 0,
-        units: 1
+        units: 0
     })
 
     //use to adjust ingredient parameters
@@ -33,8 +33,9 @@ const CreateMealForm = ({ onAddMeal, currentUserId, nextMealIndex, myFoods }) =>
     }
 
     //set the meal with updated ingredients
-    function handleAddIngredient() {
-        if (ingredient.foodId === '' || +ingredient.units <= 0) return
+    function handleAddIngredient() {       
+        if (ingredient.foodId === 0 || +ingredient.units <= 0) return
+
         const newIngredient = {
             foodId: +ingredient.foodId,
             units: +ingredient.units
@@ -55,18 +56,13 @@ const CreateMealForm = ({ onAddMeal, currentUserId, nextMealIndex, myFoods }) =>
         <option key={food.id} value={food.id}>{food.name} ({food.servingSize} {food.servingUnit})</option>
     )
 
-    const dashboard = meal.ingredients.map(item => {
-        const food = myFoods.find(food => food.id === item.foodId)
-        
-        if (!food) {
-            console.error('Food not found for ingredient:', item)
-            return null
-        }
+    // const dashboard = meal.ingredients.map(item => {
+    //     const food = myFoods.find(food => food.id === item.foodId)
 
-        return (
-            <p>{food.name} {food.servingSize} {food.servingUnit}</p>
-        )
-    })
+    //     return (
+    //         <p>{food.name} {food.servingSize} {food.servingUnit}</p>
+    //     )
+    // })
 
     return (
         <form className='form' onSubmit={handleSubmit}>
@@ -74,16 +70,16 @@ const CreateMealForm = ({ onAddMeal, currentUserId, nextMealIndex, myFoods }) =>
             <input type='text' id='name' name='name' value={meal.name} onChange={handleMealChange} required minLength='1' maxLength='26'></input>
             <label htmlFor='ingredients'>Ingredients</label>
             <select id='ingredients' name='foodId' value={ingredient.foodId} onChange={handleIngredientChange}>
-                <option value=''>Select An Ingredient</option>
+                <option value='0'>Select An Ingredient</option>
                 {ingredients}
             </select>
             <label htmlFor='units'>Units</label>
             <input type='number' id='units' name='units' value={ingredient.units} onChange={handleIngredientChange} required min='0.1' step='any'></input>
             <button type='button' className='formBtn' onClick={handleAddIngredient}>Add Ingredient</button>
             <button type='submit' className='formBtn'>Save New Meal</button>
-            <div id='dashboard'>
+            {/* <div id='dashboard'>
                 {dashboard}
-            </div>
+            </div> */}
             
         </form>
     )
