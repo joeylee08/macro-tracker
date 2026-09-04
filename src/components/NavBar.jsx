cff23338f38f68a77bc58e616dd05596ca2ee44b
Joseph <josephylee72@gmail.com>
2026-09-04 13:20:24 -0700

    navbar min-width fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,14 @@ const NavBar = () => {
 
   return (
     <nav className='navbar'>
-      <ul className='linksList'>
-        <li><NavLink to='/' className={({isActive}) => isActive ? 'selected' : null}><span>Today</span></NavLink></li>
-        <li><NavLink to='/menu' className={({isActive}) => isActive  ? 'selected' : null}><span>Menu</span></NavLink></li>
-        <li><NavLink to='/history' className={({isActive}) => isActive  ? 'selected' : null}><span>History</span></NavLink></li>
-        <li><NavLink to='/friends' className={({isActive}) => isActive  ? 'selected' : null}><span>Friends</span></NavLink></li>
-      </ul>
+      <div className='navInner'>
+        <ul className='linksList'>
+          <li><NavLink to='/' className={({isActive}) => isActive ? 'selected' : null}><span>Today</span></NavLink></li>
+          <li><NavLink to='/menu' className={({isActive}) => isActive  ? 'selected' : null}><span>Menu</span></NavLink></li>
+          <li><NavLink to='/history' className={({isActive}) => isActive  ? 'selected' : null}><span>History</span></NavLink></li>
+          <li><NavLink to='/friends' className={({isActive}) => isActive  ? 'selected' : null}><span>Friends</span></NavLink></li>
+        </ul>
+      </div>
     </nav>
   )
 }
