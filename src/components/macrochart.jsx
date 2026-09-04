f2f07496f74d5de54bbe3c67ab9311397f9b6bbe
Joseph <josephylee72@gmail.com>
2026-09-04 14:38:48 -0700

    Today responsive design

--- src/components/macrochart.jsx
+++ src/components/macrochart.jsx
@@ -12,34 +12,39 @@ const MacroChart = ({ myMealEntries }) => {
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='calories'></div>
                     </div>
+                    <h2 className='stats'>2,300 of 2,300 total</h2>
                 </div>
                 <div className='meterAndLabel'>
                     <h2 className='meterLabel'>Protein</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='protein'></div>
                     </div>
+                    <h2 className='stats'>penis of penis</h2>
                 </div>
                 <div className='meterAndLabel'>
                     <h2 className='meterLabel'>Carbs</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='carbs'></div>
                     </div>
+                    <h2 className='stats'>penis of penis</h2>
                 </div>
                 <div className='meterAndLabel'>
                     <h2 className='meterLabel'>Fat</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='fat'></div>
                     </div>
+                    <h2 className='stats'>penis of penis</h2>
                 </div>              
                 <div className='meterAndLabel'>
                     <h2 className='meterLabel'>Sodium</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='sodium'></div>
                     </div>
+                    <h2 className='stats'>penis of penis</h2>
                 </div>
             </div>
             <div className='myGoalsChild'>
-                {renderedMealEntries}
+
             </div>
         </div>
     )
