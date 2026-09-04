131ce92365a0b7ef94b96435cd7111a344f0a147
Joseph <josephylee72@gmail.com>
2026-09-04 08:49:13 -0700

    currentUserId state lifted to App level

---
+++ src/components/macrochart.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+const MacroChart = () => {
+    return (
+        <div id='myGoals'>
+            <div id='myGoalsChild'>
+                "PENIS"
+            </div>
+        </div>
+    )
+}
+
+export default MacroChart
