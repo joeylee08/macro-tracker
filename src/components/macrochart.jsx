e3162d8d424beae644f51ee806fe79f0f0980a9a
Joseph <josephylee72@gmail.com>
2026-09-04 14:54:10 -0700

    macro visuals css done

--- src/components/macrochart.jsx
+++ src/components/macrochart.jsx
@@ -8,35 +8,35 @@ const MacroChart = ({ myMealEntries }) => {
         <div className='myGoals'>
             <div className ='myGoalsChild'>
                 <div className='meterAndLabel'>
-                    <h2 className='meterLabel'>Calories</h2>
+                    <h2 className='meterLabel'>Calories (kcal)</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='calories'></div>
                     </div>
                     <h2 className='stats'>2,300 of 2,300 total</h2>
                 </div>
                 <div className='meterAndLabel'>
-                    <h2 className='meterLabel'>Protein</h2>
+                    <h2 className='meterLabel'>Protein (g)</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='protein'></div>
                     </div>
                     <h2 className='stats'>penis of penis</h2>
                 </div>
                 <div className='meterAndLabel'>
-                    <h2 className='meterLabel'>Carbs</h2>
+                    <h2 className='meterLabel'>Carbs (g)</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='carbs'></div>
                     </div>
                     <h2 className='stats'>penis of penis</h2>
                 </div>
                 <div className='meterAndLabel'>
-                    <h2 className='meterLabel'>Fat</h2>
+                    <h2 className='meterLabel'>Fat (g)</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='fat'></div>
                     </div>
                     <h2 className='stats'>penis of penis</h2>
                 </div>              
                 <div className='meterAndLabel'>
-                    <h2 className='meterLabel'>Sodium</h2>
+                    <h2 className='meterLabel'>Sodium (mg)</h2>
                     <div className='macroMeter'>
                         <div className='macroMeterFill' id='sodium'></div>
                     </div>
