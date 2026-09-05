d20436278ba3e9123dbaf2f5cb44abcd61483bcb
Joseph <josephylee72@gmail.com>
2026-09-04 19:52:31 -0700

    dynamic macroChart working well

--- src/components/macrochart.jsx
+++ src/components/macrochart.jsx
@@ -15,6 +15,8 @@ const MacroChart = ({ currentUser, myFoods, myMeals, myMealEntries }) => {
         }, { cal: 0, protein: 0, carb: 0, fat: 0, sodium: 0 }
     )
 
+    const userGoals = currentUser.macroGoals
+
     const labels = {
         cal: 'Calories (kcal)',
         protein: 'Protein (g)',
@@ -25,13 +27,15 @@ const MacroChart = ({ currentUser, myFoods, myMeals, myMealEntries }) => {
 
     //calculate from consolidated macros object
     const macroRows = Object.entries(macroTotals).map(([macro, value]) => {
+        const percentage = Math.round((value / userGoals[macro]) * 100)
+
         return (
             <div className='meterAndLabel' key={macro}>
                 <h2 className='meterLabel'>{labels[macro]}</h2>
                 <div className='macroMeter'>
-                    <div className='macroMeterFill' id={macro}></div>
+                    <div className='macroMeterFill' id={macro} style={{ width: `${Math.min(percentage, 100)}%`}}></div>
                 </div>
-                <h2 className='stats'>{value} of PENIS</h2>
+                <h2 className='stats'>{Math.round(value)} / {userGoals[macro]} [{percentage}%]</h2>
             </div>
         )
     })
