926009bf26351ab14027eee30859bbfc1b88e3c8
Joseph <josephylee72@gmail.com>
2026-09-07 09:38:35 -0700

    form submit button shadow removed

--- src/pages/friends.jsx
+++ src/pages/friends.jsx
@@ -1,9 +1,9 @@
 const Friends = () => {
     return (
-        <>
+        <div className='mainPage'>
             <h1>Your Health Buddies</h1>
             <p>What have your friends been eating?</p>
-        </>
+        </div>
     )
 }
 
--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -1,9 +1,9 @@
 const History = () => {
     return (
-        <>
+        <div className='mainPage'>
             <h1>Your Dietary History</h1>
             <p>What have you eaten?</p>
-        </>
+        </div>
     )
 }
 
