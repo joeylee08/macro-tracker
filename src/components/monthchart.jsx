7604ee23fd5a5f1f305cee4269c162c9270aabad
Joseph <josephylee72@gmail.com>
2026-09-09 09:53:16 -0700

    basic calendar dynamically generating

--- src/components/monthchart.jsx
+++ src/components/monthchart.jsx
@@ -1,11 +1,16 @@
 const MonthChart = ({ currentYearMonth, myMealEntries }) => {
     const [year, month] = currentYearMonth.split('-')
     const dayCount = new Date(year, month, 0).getDate()
+    const startDay = new Date(year, month - 1, 1).getDay()
+
     const thisMonthsEntries = myMealEntries.filter(entry => entry.dateAndTime.slice(0, 7) === currentYearMonth)    
 
-    const days = Array(dayCount).fill(0).map((entry, index) => 
-        <button key={index + 1} type='button' id='dayTile'>{index + 1}</button>
-    )
+    const days = [...Array(startDay).fill(0), ...Array(dayCount).fill(1)]
+                    .map((item, index) => 
+                        item === 0 
+                            ? <div key={`empty-${index}`} id='emptyDayTile'></div>
+                            : <button key={`day-${index}`} type='button' id='dayTile'>{index - startDay + 1}</button>
+                        )
 
     return (
         <div id='monthChart'>
