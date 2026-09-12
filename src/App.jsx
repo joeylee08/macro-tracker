9f12667ee38a0f6f4ab8895feb7a26404de90076
Joseph <josephylee72@gmail.com>
2026-09-12 15:25:08 -0700

    login signup form complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import NavBar from './components/navbar';
 import Footer from './components/footer';
 
 
-const currentUser = users[0]
+const currentUser = false
 
 function App() {
   const [myFoods, setMyFoods] = useState(foods.filter(item => item.userId === currentUser.id))
