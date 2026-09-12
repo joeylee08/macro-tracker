c8d6664dba5865299eff745d2b898b144e0b1d47
Joseph <josephylee72@gmail.com>
2026-09-12 15:10:18 -0700

    login and signup form working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { foods, meals, users, mealEntries } from './data/dummy';
 import { useState } from 'react'
+import { Navigate } from "react-router-dom";
+import Login from './pages/login'
 import Today from './pages/today';
 import Menu from './pages/menu';
 import History from './pages/history';
@@ -9,7 +11,8 @@ import Error from './pages/error';
 import NavBar from './components/navbar';
 import Footer from './components/footer';
 
-const currentUser = users[0]
+
+const currentUser = false
 
 function App() {
   const [myFoods, setMyFoods] = useState(foods.filter(item => item.userId === currentUser.id))
@@ -18,15 +21,23 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={<Today currentUser={currentUser} myFoods={myFoods} myMeals={myMeals} myMealEntries={myMealEntries} setMealEntries={setMealEntries}/>} />
-        <Route path='/menu' element={<Menu currentUser={currentUser} myFoods={myFoods} myMeals={myMeals} setMyFoods={setMyFoods} setMyMeals={setMyMeals}/>} />
-        <Route path='/history' element={<History currentUser={currentUser} myFoods={myFoods} myMeals={myMeals} myMealEntries={myMealEntries}/>} />
-        <Route path='/friends' element={<Friends currentUser={currentUser}/>} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer/>
+      {currentUser && <NavBar/>}
+        <Routes>
+          {currentUser 
+            ? (<>
+                <Route path='/' element={<Today currentUser={currentUser} myFoods={myFoods} myMeals={myMeals} myMealEntries={myMealEntries} setMealEntries={setMealEntries}/>} />
+                <Route path='/menu' element={<Menu currentUser={currentUser} myFoods={myFoods} myMeals={myMeals} setMyFoods={setMyFoods} setMyMeals={setMyMeals}/>} />
+                <Route path='/history' element={<History currentUser={currentUser} myFoods={myFoods} myMeals={myMeals} myMealEntries={myMealEntries}/>} />
+                <Route path='/friends' element={<Friends currentUser={currentUser}/>} />
+                <Route path="*" element={<Error />} />
+              </>)
+            : (<>
+                <Route path='/login' element={<Login/>} />
+                <Route path="*" element={<Navigate to='/login' replace />} />
+              </>
+            )}
+        </Routes>
+      {currentUser && <Footer/>}
     </BrowserRouter>
   )
 }
---
+++ src/pages/login.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+
+const Login = () => {
+  const [isLogin, setIsLogin] = useState(true)
+
+  const handleToggleForm = () => {
+    setIsLogin(isLogin => !isLogin)
+  }
+
+  if (isLogin) {
+    return (
+      <>
+        <form className='form loginForm' onSubmit={console.log('penis')}>
+            <button className='formBtnLogin' type='button' onClick={() => handleToggleForm()}>{isLogin ? 'to Sign Up' : 'to Login'}</button>
+            <h1>Macro Tracker Login</h1>
+            <div className='loginFormField'>
+                <label htmlFor='username'>Username:</label>
+                <input id='username' className='loginInput' type='text' onChange={console.log('penis')} value={'poo'} placeholder="Enter Username"></input>
+                <label htmlFor='password'>Password:</label>
+                <input id='password' className='loginInput' type='password' onChange={console.log('penis')} value={'poo'} placeholder="Enter Password"></input>
+            </div>
+            <div id='loginButtons'>
+                <button className='formBtnLogin' type='submit'>LOGIN</button>
+            </div>
+        </form>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <form className='form loginForm' onSubmit={console.log('penis')}>
+            <button className='formBtnLogin' type='button' onClick={() => handleToggleForm()}>{isLogin ? 'to Sign Up' : 'to Login'}</button>
+            <h1>Macro Tracker Sign Up</h1>
+            <div className='loginFormField'>
+                <label htmlFor='username'>Username:</label>
+                <input id='username' className='loginInput' type='text' onChange={console.log('penis')} value={'poo'} placeholder="Enter Username"></input>
+                <label htmlFor='email'>Email:</label>
+                <input id='email' className='loginInput' type='email' onChange={console.log('penis')} value={'poo'} placeholder="Enter Email"></input>
+                <label htmlFor='password'>Password:</label>
+                <input id='password' className='loginInput' type='password' onChange={console.log('penis')} value={'poo'} placeholder="Enter Password"></input>
+                <label htmlFor='confirmPassword'>Confirm Password:</label>
+                <input id='confirmPassword' className='loginInput' type='password' onChange={console.log('penis')} value={'poo'} placeholder="Confirm Password"></input>
+            </div>
+            <div id='loginButtons'>
+                <button className='formBtnLogin' type='submit'>SIGNUP</button>
+            </div>
+        </form>
+      </>
+    )
+  }
+  
+}
+
+export default Login
